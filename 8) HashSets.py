d7f0ef81f3b725455cd40f8fc84776286b063b93
debarghya-choudhury <debarghya.choudhury@yahoo.com>
2024-03-10 08:14:55 +0530

    learned hashset intersection method and hashset comprehension

---
+++ 8) HashSets.py
@@ -0,0 +1,26 @@
+# HashSet
+mySet = set()
+
+mySet.add(1)
+mySet.add(2)
+print(mySet)
+print(len(mySet))
+
+print(1 in mySet)
+print(2 in mySet)
+print(3 in mySet)
+
+mySet.remove(2)    # **********
+print(2 in mySet)
+
+# list to set
+print(set([1, 2, 3]))
+
+# Set comprehension
+mySet = { i for i in range(5) }
+print(mySet)
+
+# Intersection
+set1 = set([1,2,2,1])
+set2 = set([2,2])
+intersection = set1.intersection(set2)  # >>> {2}
